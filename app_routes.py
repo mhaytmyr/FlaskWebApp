db6550df45f1fbc395f8023a98bc656f854dda25
maksat <mhaytmuradov@gmail.com>
2019-01-18 07:19:52 -0600

    added method to preprocess dcm

--- app_routes.py
+++ app_routes.py
@@ -3,6 +3,26 @@ import os
 from application import app, APP_ROOT
 from flask import render_template, request, url_for
 
+import cv2, numpy as np
+import pydicom
+
+def convert_dcm_to_image(dst):
+    #read dcm image
+    img = pydicom.read_file(dst).pixel_array.astype('float32');
+    shape = img.shape;
+    #rescale image
+    imgScaled = (img-img.min())*255/img.max();
+    #convert to uint
+    imgScaled = np.uint(imgScaled);
+    #write image as png file
+    newFileName = dst.replace('dcm','png'); 
+    cv2.imwrite(newFileName,imgScaled);
+    print("Img saved! ",img.shape)
+
+    return newFileName.split('/')[-1]#I need just file name not the path
+    
+    
+
 @app.route('/',methods=['POST','GET'])
 def index():
     return render_template("predict_bootsrap.html",filenames=None)
@@ -20,15 +40,18 @@ def upload_method():
     filenames = [];
     #loop over the files
     for file in request.files.getlist("input-file"):
-        print(file)
+        #print(file)
         filename = file.filename
-        filenames.append(filename)
+        #filenames.append(filename)
 
         #destination to upload
         dst = "/".join([target,filename])
 
         #save that file to specified destination
-        #TODO: add preprocessing here to DCM images
         file.save(dst)
+
+        #add preprocessing here to DCM images
+        filename = convert_dcm_to_image(dst);
+        filenames.append(filename);
 
     return render_template("predict_bootsrap.html",filenames=filenames)
