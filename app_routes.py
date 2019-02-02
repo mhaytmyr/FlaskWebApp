d33cbeb6e3be67532338a77c9a38e80ee405d8b6
maksat <mhaytmuradov@gmail.com>
2019-02-02 17:59:35 -0600

    adding multi-submit functionality

--- app_routes.py
+++ app_routes.py
@@ -6,6 +6,9 @@ from flask import render_template, request, url_for
 import cv2, numpy as np
 import pydicom
 
+FILENAMES = None
+MODELNAME = None
+
 def convert_dcm_to_image(dst):
     #read dcm image
     img = pydicom.read_file(dst).pixel_array.astype('float32');
@@ -20,10 +23,60 @@ def convert_dcm_to_image(dst):
     print("Img saved! ",img.shape)
 
     return newFileName.split('/')[-1]#I need just file name not the path
+
+def get_file_names(request):
+    #destination to save temporary images
+    target = os.path.join(APP_ROOT,'static/')
     
-    
+    #if destination doesn't exist create new destination
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    filenames = [];
+    #loop over the files
+    for file in request.files.getlist("input_files"):
+        #print(file)
+        filename = file.filename
+        #filenames.append(filename)
+
+        #destination to upload
+        dst = "/".join([target,filename])
+
+        #save that file to specified destination
+        file.save(dst)
+
+        #add preprocessing here to DCM images
+        filename = convert_dcm_to_image(dst)
+        filenames.append(filename)
+
+    return filenames
+
 
 @app.route('/',methods=['POST','GET'])
+def main_test():
+    global FILENAMES
+
+    #check landing page
+    if request.method == 'POST':
+        if request.form['action']=='UPLOAD':
+            #get uploaded filenames
+            filenames = get_file_names(request)
+            FILENAMES = filenames
+            return render_template("predict_bootsrap.html",filenames=filenames)
+        elif request.form['action']=='SEGMENT':
+            tmpPage = '<h1>Segment pressed</h1>'
+            for key in request.form:
+                tmpPage += '<p>form '+key+':'+request.form[key]+'</p>'
+            #print filenames for debugging
+            for file in FILENAMES:
+                tmpPage += '<p>'+file+'</p>'
+            return tmpPage
+        else:
+            return render_template("predict_bootsrap.html",filenames=None)
+
+    return render_template("predict_bootsrap.html",filenames=None)
+
+@app.route('/tmp',methods=['POST','GET'])
 def index():
     return render_template("predict_bootsrap.html",filenames=None)
 
@@ -54,4 +107,17 @@ def upload_method():
         filename = convert_dcm_to_image(dst);
         filenames.append(filename);
 
+
+    #update file names
+
     return render_template("predict_bootsrap.html",filenames=filenames)
+
+@app.route('/predict',methods=['POST','GET'])
+def model_selection():
+    print("Calling model_selection")
+
+    for key in request.form:
+        print("form key "+key+":"+request.form[key])
+    #print("Success called model selection")
+    #return redirect(url_for('upload_method'))
+    return render_template("predict_bootsrap.html",filenames=FILENAMES)
